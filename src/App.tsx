d442a74cc66a6b9afae29ed0dda05b81ac0348b5
Steve Puma <102320035+StevePuma@users.noreply.github.com>
2023-08-31 14:13:17 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BarChart, ColumnChart, ExecuteQuery } from "@sisense/sdk-ui";
+import { Data, measures } from "@sisense/sdk-data";
+import * as DM from "./sample-ecommerce";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ExecuteQuery
+        dataSource={DM.DataSource}
+        dimensions={[DM.Commerce.Date.Years]}
+        measures={[measures.sum(DM.Commerce.Revenue, 'Total Quantity')]}
+        filters={[]}
+      >
+        {(data: Data) => {
+          return <BarChart dataSet={data}
+                           dataOptions={{
+                             category: [{name: 'Years', type: 'datetime'}],
+                             value: [{name: 'Total Quantity'}],
+                             breakBy: []
+                           }}
+                  />;
+        }}
+      </ExecuteQuery>
+    </>
   );
 }
 
